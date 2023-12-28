9cd7ea0731f182e30367fadcff15f3aebfb1c5e5
nohimgodage <nohim.godage@gmail.com>
2023-12-28 15:42:33 +0530

    Render single post details with very simple markdown & dummy data

---
+++ src/components/posts/post-detail/post-content.tsx
@@ -0,0 +1,19 @@
+import ReactMarkdown from 'react-markdown';
+
+import PostHeader from './post-header';
+import classes from './post-content.module.css';
+
+const PostContent = (props:any) =>{
+    const { post } = props;
+
+    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+    return (
+        <article className={classes.content}>
+            <PostHeader title={post.title} image={imagePath} />
+            <ReactMarkdown>{post.content}</ReactMarkdown>
+        </article>
+    );
+}
+
+export default PostContent;
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,8 +1,29 @@
-const PostDetails = () => {
+import Head from 'next/head';
+
+import PostContent from '../../components/posts/post-detail/post-content';
+import {DUMMY_POSTS} from "@/pages";
+
+const PostDetailPage = (props:any) => {
     return (
         <>
-            <p>Post Details</p>
+            <Head>
+                <title>{props.post.title}</title>
+                <meta name='description' content={props.post.excerpt} />
+            </Head>
+            <PostContent post={props.post} />
         </>
     );
 }
-export default PostDetails;
+
+export function getServerSideProps(){
+    return {
+        props: {
+            post:{
+                ...DUMMY_POSTS[0],
+                content: '# This is a first post.'
+            }
+        }
+    };
+}
+
+export default PostDetailPage;
