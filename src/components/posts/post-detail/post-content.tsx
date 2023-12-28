a335459bce7d5dfa98a8db7c6122e08f379262c6
nohimgodage <nohim.godage@gmail.com>
2023-12-28 19:03:36 +0530

    Adding custom highlighting to MarkDown Content

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -1,4 +1,7 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -8,10 +11,56 @@ const PostContent = (props:any) =>{
 
     const imagePath = `/images/posts/${post.image}`;
 
+    const customRenderers = {
+        // img(image: any) {
+        //   return (
+        //     <Image
+        //       src={`/${image.src}`}
+        //       alt={image.alt}
+        //       width={600}
+        //       height={300}
+        //     />
+        //   );
+        // },
+        p(paragraph:any) {
+            const { node } = paragraph;
+
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`${image.properties.src}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                );
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+
+        code(code: any) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter
+                    style={darcula}
+                    language={language}
+                >
+                    {children}
+                </SyntaxHighlighter>
+            );
+        },
+    };
+
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
         </article>
     );
 }
