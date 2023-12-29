2f4bd73c7d9e2f284c58f79e41ea897b7b29763d
nohimgodage <nohim.godage@gmail.com>
2023-12-29 11:16:14 +0530

    ES Lint Exception

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -1,6 +1,7 @@
 import classes from './logo.module.css';
 
 const Logo = () => {
+    // eslint-disable-next-line react/no-unescaped-entities
     return <div className={classes.logo}>Nohim's Next Blog</div>;
 }
 
